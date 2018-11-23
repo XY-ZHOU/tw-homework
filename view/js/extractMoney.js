3607c48ccb3a93559fb9c891366700e75756546d
XY-ZHOU <731986251@qq.com>
2018-11-23 19:06:11 +0800

    delete other history dir

--- view/js/extractMoney.js
+++
@@ -1,6 +0,0 @@
-const money = '￥20';
-function extractAmount(money){
-  let numberOfMoney = money.substring(1, money.length);
-  return numberOfMoney;
-}
- document.write(extractAmount(money));
--- view/js/uppearcaseString.js
+++
@@ -1,6 +0,0 @@
-const name = 'hello';
-function uppercaseStr(str){
-  let resStr = str.toUpperCase();
-  return resStr;
-}
-document.write(uppercaseStr(name));
